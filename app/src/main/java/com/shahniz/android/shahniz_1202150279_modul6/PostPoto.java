e44f94e1f437353633d5ba7cbd6f0b39681a0b07
unknown <skaulika@gmail.com>
2018-04-01 23:56:24 +0700

    first commit

---
+++ app/src/main/java/com/shahniz/android/shahniz_1202150279_modul6/PostPoto.java
@@ -0,0 +1,17 @@
+package com.shahniz.android.shahniz_1202150279_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PostPoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_poto);
+    }
+
+    public void uploadgambar(View view) {
+    }
+}
